be5d6ab306e372620c44308f781f63b5a3de9add
Dan Swick <dan.swick@gmail.com>
2016-09-12 23:09:36 -0500

    add itinerary page

---
+++ itinerary/itinerary-all.js
@@ -0,0 +1,282 @@
+var agenda = {
+  "name": "",
+  "role": "",
+  "agenda": [
+    {
+      "time": "08:00",
+      "roles": {
+        "Erin": "Wake up and go for a run",
+        "Dan": "Wake up and go for a run",
+        "Jason": "Walk Basel"
+      }
+    },
+    {
+      "time": "08:30",
+      "roles": {
+      	"Erin": "Exercise!",
+      	"Dan": "Exercise!"
+      }
+    },
+    {
+      "time": "09:00",
+      "roles": {
+      	"Erin": "Shower and pack to leave"
+      }
+    },
+    {
+      "time": "09:30",
+      "roles": {
+      	"Erin": "Leave for getting ready party"
+      }
+    },
+    {
+      "time": "10:00",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "10:30",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "11:00",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "11:30",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "12:00",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "12:30",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "13:00",
+      "roles": {
+      	"Anna": "Arrive at Trigger to start setting up decorations", 
+      	"Brittany": "Arrive at Trigger to start setting up decorations", 
+      	"Jenny": "Arrive at Trigger to start setting up decorations", 
+      	"April": "Arrive at Trigger to start setting up decorations", 
+      	"Nikki": "Arrive at Trigger to start setting up decorations", 
+      	"Rob": "Arrive at Trigger to start setting up decorations", 
+      	"Peggy": "Arrive at Trigger to start setting up decorations", 
+      	"Aubrie": "Arrive at Trigger to start setting up decorations", 
+      	"Caryn": "Arrive at Trigger to start setting up decorations"
+      }
+    },
+    {
+      "time": "13:30",
+      "roles": {
+      	"Paul": "Arrive at Trigger to drop off booze",
+      	"Peggy": "Sign off with Paul on Booze and Mariano's on ice"
+      }
+    },
+    {
+      "time": "13:45",
+      "roles": {
+      	"Erin": "Arrive at Palmer Square",
+      	"Dan": "Arrive at Palmer Square",
+      	"Melissa": "Arrive at Palmer Square"
+      }
+    },
+    {
+      "time": "14:00",
+      "roles": {
+      	"Erin": "D&E photos",
+      	"Dan": "D&E photos",
+      	"Melissa": "D&E photos"
+      }
+    },
+    {
+      "time": "14:30",
+      "roles": {
+      	"Cody": "Arrive at Palmer Square, wedding party photos",
+      	"Jason": "Arrive at Palmer Square, wedding party photos",
+      	"Mo": "Arrive at Palmer Square, wedding party photos"
+      }
+    },
+    {
+      "time": "15:00",
+      "roles": {
+      	
+      }
+    },
+    {
+      "time": "15:30",
+      "roles": {
+      	"Nikki": "Leave from Trigger with chairs, metal buckets, and baby's breath",
+      	"Rob": "Leave from Trigger with chairs, metal buckets, and baby's breath"
+      }
+    },
+    {
+      "time": "16:00",
+      "roles": {
+      	"Andy": "Arrive at Palmer Square with backdrop",
+      	"Brian": "Arrive at Trigger with guitar",
+      	"Erin": "Find a place to hide until the ceremony begins",
+      	"Dan": "Find a place to hide until the ceremony begins",
+      	"Cody": "Start setting up chairs in 3/4 circle, set up backdrop, metal buckets with baby's breath",
+      	"Jason": "Start setting up chairs in 3/4 circle, set up backdrop, metal buckets with baby's breath",
+      	"Mo": "Start setting up chairs in 3/4 circle, set up backdrop, metal buckets with baby's breath", 
+      	"Peggy": "Arrive at Palmer Square"
+      }
+    },
+    {
+      "time": "16:30",
+      "roles": {
+      	"Guests": "Ceremony begins"
+      }
+    },
+    {
+      "time": "17:00",
+      "roles": {
+      	"Guests": "Ceremony ends",
+      	"Erin": "Family photos",
+      	"Dan": "Family photos",
+      	"Andy": "Family photos",
+      	"Dianne": "Family photos",
+      	"Bill": "Family photos",
+      	"Tracy": "Family photos",
+      	"Nikki": "Family photos",
+      	"Jay": "Family photos",
+      	"Rob": "Family photos",
+      	"Nicole": "Family photos",
+      	"Melissa": "Family photos",
+      	"Peggy": "Head up to Trigger",
+      	"Anna": "Head up to Trigger"
+      }
+    },
+    {
+      "time": "17:30",
+      "roles": {
+      	"Event staff": "Arrive at Trigger",
+      	"Lorenzo": "Arrive at Trigger",
+      	"Peggy": "Help event staff and Lorenzo find their set up areas",
+      	"Anna": "Help event staff and Lorenzo find their set up areas"
+      }
+    },
+    {
+      "time": "17:45",
+      "roles": {
+      	"Big star": "Food arrives",
+      	"Event staff": "Receive food and begin setting up"
+      }
+    },
+    {
+      "time": "18:00",
+      "roles": {
+      	"Event staff": "Bar set up complete",
+      	"Guests": "Guests start arriving (probably)",
+      	"Lorenzo": "Kick off background music"
+      }
+    },
+    {
+      "time": "18:30",
+      "roles": {
+      	"Guests": "Reception officially begins",
+      	"Event staff": "Start cocktail service (earlier is ok if people are ready)"
+      }
+    },
+    {
+      "time": "19:00",
+      "roles": {
+      	"Guests": "Dinner starts",
+      	"Event staff": "Dinner starts",
+      	"Erin": "Erin & Dan arrive",
+      	"Dan": "Erin & Dan arrive"
+      }
+    },
+    {
+      "time": "19:30",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "20:00",
+      "roles": {
+      	"Guests": "Speeches start",
+      	"Lorenzo": "Speeches start. Mic over the main PA system",
+      	"Bill": "Speech!",
+      	"Dianne": "Speech!",
+      	"Jason": "Speech!",
+      	"Mo": "Speech!"
+      }
+    },
+    {
+      "time": "20:30",
+      "roles": {
+      	"Guests": "Pie! (if speeches are done)",
+      	"Event staff": "Pie! (if speeches are done)"
+      }
+    },
+    {
+      "time": "21:00",
+      "roles": {
+      	"Guests": "Dancing starts!",
+		 		"Lorenzo": "Kick off the dance party as soon as people seem done with dessert. Start with Erin & Dan's first dance, then father/daughther & mother/son",
+		 		"Erin": "First dance with Dan, then father/daughter dance with Bill",
+		 		"Dan": "First dance with Erin, then mother/son dance with Dianne",
+		 		"Bill": "Father/daughter dance",
+		 		"Dianne": "Mother/son dance"
+      }
+    },
+    {
+      "time": "21:30",
+      "roles": {
+
+      }
+    },
+    {
+      "time": "22:00",
+      "roles": {
+
+      }
+    },
+    {
+    	"time": "22:30",
+    	"roles": {
+
+    	}
+    },
+    {
+    	"time": "23:00",
+    	"roles": {
+    		"Guests": "One last call for alcohol so finish your whiskey or beer.",
+    		"Event staff": "Last call!"
+    	}
+    },
+    {
+      "time": "23:30",
+      "roles": {
+      	"Lorenzo": "Dancing ends and we say goodnight."
+      }
+    },
+    {
+      "time": "24:00",
+      "roles": {
+      	"Brittany": "Take pie pans home",
+      	"Anna": "Take decorations home",
+      	"Jay": "Take beverage servers home",
+      	"Katelyn & Sarah": "Take service stuff home",
+      	"Bill": "Take any physical gifts home",
+        "Everyone": "Feel the love"
+      }
+    }
+  ]
+}
